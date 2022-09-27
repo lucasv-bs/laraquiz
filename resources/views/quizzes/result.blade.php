c1d4e45f22df0512984b9ab3df1011bb79f94546
Lucas Vinicius <lucasvinicius.bs03@gmail.com>
2022-09-27 13:40:01 -0300

    Change the quiz execution result page

--- resources/views/quizzes/result.blade.php
+++ resources/views/quizzes/result.blade.php
@@ -3,20 +3,21 @@
 @section('content')
 <div class="container px-4">
     <div class="row gx-5 gy-4">
-        <x-card class="text-center">
+        <x-card class="col text-center">
             <h2>You finished the Quiz! Here is your result.</h2>
-            <div>
+            <div class="row">
                 <p class="fs-4">{{$result}}</p>
             </div>
-            <div>
+            <div class="row">
                 <img 
                     src="{{$quiz->cover_image
                         ? Storage::url($quiz->cover_image)
                         : Storage::url('quizzes/cover-images/no-image.jpg')
                     }}" 
+                    class="img-fluid rounded"
                     alt="Quiz cover image">
             </div>
-            <p class="fs-4">Would you like take another Quiz? <a href="/quizzes">Click here</a></p>
+            <p class="fs-4 mt-3">Would you like take another Quiz? <a href="/quizzes">Click here</a></p>
         </x-card>
     </div>
 </div>
